dbda8aa1938d386f9caf29da295f2d17fa68fac8
Artem Baikov <artem.baikov.darkmoon@gmail.com>
2024-05-16 19:28:52 +0300

    Initial commit

---
+++ lib/with_filters.ex
@@ -0,0 +1,18 @@
+defmodule WithFilters do
+  @moduledoc """
+  Documentation for `WithFilters`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WithFilters.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/with_filters_test.exs
@@ -0,0 +1,8 @@
+defmodule WithFiltersTest do
+  use ExUnit.Case
+  doctest WithFilters
+
+  test "greets the world" do
+    assert WithFilters.hello() == :world
+  end
+end
